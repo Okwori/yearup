790b8c2d18009f3d55a142d123e6229e4492da7c
pringwa <ayegba_24_pass>
2020-04-24 20:11:20 +0200

    dynamically set cut-off ratio

--- src/clj/yearup/db/queries.clj
+++ src/clj/yearup/db/queries.clj
@@ -29,12 +29,13 @@
   "Persists the result of the quiz for the specified user and a collection of options"
   [user-id options-coll]
   (let [q (map #(db/create-answer! {:option-id % :user-id user-id}) options-coll)
+        q1 (Integer/parseInt (:value (db/get-setting {:name "RATIO"})))
         q2 (if (every? #(= % 1) q) (filter #(not (nil? %)) (map #(db/get-option-by-sentiment-not-null {:id %}) options-coll)))
         q3 (map #(:sentiment %) q2)
         q4 (* (/ (apply + q3) (count q3)) 100)]
     (cond
-      (>= q4 60) {:data {:message "Seems like Year Up could be a great fit for you." :code 1}}
-      (< 60) {:data {:message "Ok, now might not be the best time for you to start." :code 2}})))
+      (>= q4 q1) {:data {:message "Seems like Year Up could be a great fit for you." :code 1}}
+      (< q1) {:data {:message "Ok, now might not be the best time for you to start." :code 2}})))
 
 (defn get-quiz
   "Gets a quiz or a vector of quizzes"
