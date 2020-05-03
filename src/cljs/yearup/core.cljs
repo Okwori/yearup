ba731dcf0d2861ebf4dbdf7437e7870f8fabf5d1
pringwa <ayegba_24_pass>
2020-05-03 13:22:13 +0200

    Completely update frontend quiz flow

--- src/cljs/yearup/core.cljs
+++ src/cljs/yearup/core.cljs
@@ -1,85 +1,93 @@
 (ns yearup.core
   (:require
+    [cljs.spec.alpha :as s]
+    [clojure.string :as string]
     [day8.re-frame.http-fx]
-    [reagent.core :as r]
-    [re-frame.core :as rf]
     [goog.events :as events]
     [goog.history.EventType :as HistoryEventType]
     [markdown.core :refer [md->html]]
-    [yearup.ajax :as ajax]
-    [yearup.events]
+    [reagent.core :as r]
     [reitit.core :as reitit]
     [reitit.frontend.easy :as rfe]
-    [clojure.string :as string])
+    [re-frame.core :as rf]
+    [yearup.ajax :as ajax]
+    [yearup.events])
   (:import goog.History))
 
-;(defn nav-link [uri title page]
-;  [:a.navbar-item
-;   {:href  uri
-;    :class (when (= page @(rf/subscribe [:page])) :is-active)}
-;   title])
+(s/def ::email (s/and string? (partial re-matches #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")))
 
-;(defn navbar []
-;  (r/with-let [expanded? (r/atom false)]
-;              [:nav.navbar.is-info>div.container
-;               [:div.navbar-brand
-;                [:a.navbar-item {:href "/" :style {:font-weight :bold}} "YearUp"]
-;                [:span.navbar-burger.burger
-;                 {:data-target :nav-menu
-;                  :on-click    #(swap! expanded? not)
-;                  :class       (when @expanded? :is-active)}
-;                 [:span] [:span] [:span]]]
-;               [:div#nav-menu.navbar-menu
-;                {:class (when @expanded? :is-active)}
-;                [:div.navbar-start
-;                 [nav-link "#/" "Home" :home]
-;                 [nav-link "#/about" "About" :about]]]]))
-
-(defn about-page []
-    ;[:section.section>div.container>div.content
-    ; [:img {:src "/img/warning_clojure.png"}]]
-
-  )
+(defn admin-page []   )                                     ;TODO!
 
 (defn home-page []
-  ;[:section.section>div.container>div.content
-  ; (when-let [docs @(rf/subscribe [:docs])]
-  ;   [:div {:dangerouslySetInnerHTML {:__html (md->html docs)}}])]
-  [:div.container-content
-  (when-let [data @(rf/subscribe [:quiz])]
-    (let [question (first (get-in data [:data :questions]))
-          cities (get-in data [:data :cities])]
-      [:div.outside-div
-       {:style {:background-image  (str "linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url('" (str "img/" (:backgroundImage question)) "')" )
-                :background-repeat "no-repeat" :background-size "cover" :background-position "50% 25%" :background-attachment "fixed"}}
-       [:div.middle-div [:img.logo {:src "img/YU_goldwhiteCC.png"}]
-        [:div.inside-div
-         [:p.lg-text (:detail question)]
-         (cond
-           (= (:order question) 1) [:div.dropdown.show.btn-container-dropdown
-                                    [:a#dropdownMenuLink.btn.btn-secondary.dropdown-toggle.btn-dropdown-size
-                                     {:href "#" :role "button" :data-toggle "dropdown" :aria-haspopup "true"} (:name (first (:options question)))]
-                                    [:div.dropdown-menu.dropdown-display {:aria-labelledby "dropdownMenuLink" }
-                                     (map (fn [n]
-                                            [:a.dropdown-item {:href "#" :id (str (random-uuid)) :key (:id n)  } (:name n)]) cities)
-                                                 ;[:a.dropdown-item {:href "#"} ]
-                                                 ;[:a#los-angeles.dropdown-item {:href "#"} "Los Angeles"]
-                                                 ;[:a#new-york.dropdown-item {:href "#"} "New York"]
-                                                 ;[:a#charlotte.dropdown-item {:href "#"} "Charlotte"]
-                                     ]]
-           (= (:order question) 2)
-           "")
-         ]]]))])
+  (when-let [quiz @(rf/subscribe [:quiz])]
+    (let [question (first (get-in quiz [:data :questions]))
+          cities (get-in quiz [:data :cities])
+          order (:order question)
+          answer @(rf/subscribe [:answers])]
+      [:div [:div#wrapper [:img.yearuplogo {:src "img/YU_goldwhiteCC.png" :alt "logo"}]
+            [:header#header
+             [:div.logo
+              [:img.logo.icon {:src "img/Y-only-gold.png" :alt "logo-icon"}]]
+             [:div.content
+              [:div.inner
+               (if (= order 13)
+                 (do (when-let [city-id (get-in answer [:city :id])]
+                         (condp = city-id
+                           1 [:h1 [:strong (str (:detail question) " Culver City in West Los Angeles")]]
+                           2 [:h1 [:strong (str (:detail question) " one of 3 locations: Wall St., NYC, TriBeCa, NYC, Jersey City, NJ")]]
+                           3 [:h1 [:strong (str (:detail question)" Uptown Charlotte")]])))
+                 [:h1 [:strong (:detail question)]])
+               (if (or (= order 12) (= order 13)) (do [:br][:br]) (do [:br][:br][:br]))
+               (if (= order 1)
+                 [:h3 (:name (first (:options question)))]
+                 [:h3 (:subNote question)])]
+              (when-let [error @(rf/subscribe [:common/error])]
+                [:h4 {:style {:color "red"}} (str error)])]
+             [:nav
+              [:ul (cond (= order 1)
+                         (for [city cities]
+                           [:li [:a {:key (:id city) :on-click #(rf/dispatch [:submit city])} (:name city)]])
 
-(defn quiz-page [])
+                         (= order 2)
+                         [:li [:a {:style {:background-color "#AAA4A2"} :on-click #(rf/dispatch [:submit])} (:name (first (:options question)))]]
+
+                         (= order 3)
+                         (let [vals-email (r/atom {:email ""})]
+                           [:li [:div.fields
+                                [:div.field.half
+                                 [:input#user-email {:type        "email" :name        "user-email"  :required    "required"
+                                                     :placeholder "email@example.com"
+                                                     :on-change   #(swap! vals-email assoc :email (-> % .-target .-value))}]]
+                                [:a.button.primary {:on-click #(if (or (not (s/valid? ::email (:email @vals-email))) (nil? (:email @vals-email)) (empty? (:email @vals-email)))
+                                                                 (rf/dispatch [:common/set-error "Enter a valid email address"])
+                                                                 (rf/dispatch [:submit-user @vals-email]))} "Submit"]]])
+
+                         (->> (into [] (range 4 14))  (some #(= order %)))
+                         (for [option (:options question)]
+                           [:li [:a.button {:key      (:id option)
+                                            :style    (cond (= (:sentiment option) 0) {:background-color "#E11D0D"}
+                                                            (string/includes? (:name option) "Not sure") {:background-color "#AAA4A2"}
+                                                            (= option (first (:options question))) {:background-color "#27760D"})
+                                            :on-click (if (= order 13 )
+                                                        #(rf/dispatch [:submit-answers option])
+                                                        #(rf/dispatch [:submit option]))}
+                                 (:name option)]])
+
+                         (= order 0) [:li [:a.button {:href "https://www.yearup.org/seize-opportunity/"
+                                                      :style {:background-color "#AAA4A2"}}
+                                           (:name (first (:options question)))]])]]]
+             (if (and (= order 0) (string/includes? (:detail question) "Ok, now might")) [:a {:href "https://www.yearup.org/seize-opportunity/"} "Wait, I still want to learn more about Year Up"])
+             [:footer#footer
+              [:p.copyright "© 2020 YearUp"]]]
+       [:div#bg {:style {:background-position "center" :background-size "cover" :background-repeat "no-repeat" :z-index "1"
+                        :background-image  (str "url('img/", (if (= order 13)
+                                                               (do (when-let [city-id (get-in answer [:city :id])]
+                                                                     (condp = city-id 1 "losAngeles.jpg" 2 "newYork.jpg" 3 "charlotte.jpg")))
+                                                               (:backgroundImage question)), "')")}}]])))
 
 (defn page []
   (if-let [page @(rf/subscribe [:page])]
-    ;[:div
-     ;[navbar]
-     [page]
-;]
-))
+     [page]))
 
 (defn navigate! [match _]
   (rf/dispatch [:navigate match]))
@@ -90,10 +98,7 @@
            :view        #'home-page
            :controllers [{:start (fn [_] (rf/dispatch [:page/init-home]))}]}]
      ["/about" {:name :about
-                :view #'about-page}]
-     ["/quiz" {:name :quiz
-               :view #'quiz-page
-               :controllers [{:start (fn [_] (rf/dispatch [:quiz/start]))}] }]]))
+                :view #'admin-page}]]))
 
 (defn start-router! []
   (rfe/start!
--- src/cljs/yearup/events.cljs
+++ src/cljs/yearup/events.cljs
@@ -26,16 +26,18 @@
     {:navigate-fx! [url-key params query]}))
 
 (rf/reg-event-db
-  :set-quiz
-  (fn [db [_ docs]]
-    (assoc db :quiz docs)))
+  :common/set-error
+  (fn [db [_ error]]
+    (if (contains? error :status-text)
+      (do (if (= (:status-text error) "Server Error")
+            (assoc db :common/error "You have already used this email")
+            (assoc db :common/error (:status-text error))))
+      (assoc db :common/error error))))
 
-(rf/reg-event-fx
-  :slide-one
-  (fn [db _] (assoc db :hey (inc 1)))
-  ;(fn [{:keys [db]} [_ id]]                                            ;(assoc db :hey id)
-  ;  {:db (assoc db :hey id)})
-  )
+(rf/reg-event-db
+  :set-quiz
+  (fn [db [_ quiz]]
+    (assoc db :quiz quiz)))
 
 (rf/reg-event-fx
   :fetch-quiz
@@ -45,25 +47,92 @@
                   :params           {:quizId 1}
                   :format          (ajax/json-request-format)
                   :response-format (ajax/transit-response-format)
-                  :on-success      [:set-quiz]}}))
-
-;(rf/reg-event-fx
-;  :fetch-cities
-;  (fn [_ _]
-;    {:http-xhrio {:method          :get
-;                  :uri             "/api/v1/cities"
-;                  :format          (ajax/json-request-format)
-;                  :response-format (ajax/transit-response-format)
-;                  :on-success      [:set-cities]}}))
-
+                  :on-success      [:set-quiz]
+                  :on-failure      [:common/set-error]}} ))
 (rf/reg-event-db
-  :common/set-error
-  (fn [db [_ error]]
-    (assoc db :common/error error)))
+  :clear-exceptions
+  (fn [db _]
+    (dissoc db :common/error)))
 
-;(rf/reg-event-fx
-;  :quiz/start
-;  (fn [_ _] ))
+(rf/reg-event-fx
+  :next-slide
+  (fn [{:keys [db]} [old-question new-question]]
+    (if (not= old-question new-question)
+      {:db (assoc-in db [:quiz :data :questions]
+                     (filterv #(not= % new-question)
+                              (get-in db [:quiz :data :questions])))
+       :dispatch [:clear-exceptions]})))
+
+(rf/reg-event-fx
+  :submit
+  (fn [{:keys [db]} [_ input]]
+    {:db (cond (nil? input) db
+               (contains? input :addresses) (assoc-in db [:answers :city] input)
+               (and (= (count input) 1) (contains? input :id))(assoc-in db [:answers :user] {:user-id (:id input)
+                                                                                             :email   (get-in db [:answers :user :email])})
+               (and (not (nil? input))  (contains? input (and :id :name :sentiment))) (assoc-in db [:answers :selections]
+                                                                                                (conj (get-in db [:answers :selections])
+                                                                                                      (:id input))))
+     :dispatch [:next-slide (first (get-in db [:quiz :data :questions]))]}))
+
+(rf/reg-event-fx
+  :create-user
+  (fn [{:keys [db]} [_ email]]
+    {:http-xhrio {:method          :post
+                  :uri             "/api/v1/user/create"
+                  :params          {:email (:email email)}
+                  :format          (ajax/json-request-format)
+                  :response-format (ajax/transit-response-format)
+                  :on-success      [:submit]
+                  :on-failure      [:common/set-error]}} ))
+
+(rf/reg-event-fx
+  :submit-user
+  (fn [{:keys [db]} [_ email]]
+    {:db       (assoc-in db [:answers :user] email)
+     :dispatch [:create-user email]}))
+
+(rf/reg-event-fx
+  :finish
+  (fn[{:keys [db]} [_ result]]
+    {:db       (assoc-in db [:quiz :data :questions]
+                         (conj (get-in db [:quiz :data :questions])
+                               {:id              0
+                                :detail          (cond (= (get-in result [:data :code]) 1) "Seems like Year Up could be a great fit for you."
+                                                       (= (get-in result [:data :code]) 2) "Ok, now might not be the best time for you to start.")
+                                :subNote         (cond (= (get-in result [:data :code]) 1) "Our next class starts this fall, and you can apply today."
+                                                       (= (get-in result [:data :code]) 2) "Someone from Year Up would be happy to follow up and answer any of your questions.")
+                                :backgroundImage "learnMore.jpg"
+                                :order           0
+                                :options         [{:id        0
+                                                   :name      (cond (= (get-in result [:data :code]) 1) "LEARN MORE"
+                                                                    (= (get-in result [:data :code]) 2) "CONTACT ME")
+                                                   :sentiment nil}]}))
+     :dispatch [:next-slide (first (get-in db [:quiz :data :questions]))]} ))
+
+(rf/reg-event-fx
+  :create-answer
+  (fn [{:keys [db]} _]
+    {:http-xhrio {:method          :post
+                  :uri             "/api/v1/submit"
+                  :params          {:userId (get-in db [:answers :user :user-id])
+                                    :selections (get-in db [:answers :selections])
+                                    :cityId (get-in db [:answers :city :id])}
+                  :format          (ajax/json-request-format)
+                  :response-format (ajax/transit-response-format)
+                  :on-success      [:finish]
+                  :on-failure      [:common/set-error]}}))
+
+(rf/reg-event-fx
+  :push-answer
+  (fn [_ _]
+    {:dispatch [:create-answer]}))
+
+(rf/reg-event-fx
+  :submit-answers
+  (fn [{:keys [db]} [_ option]]
+    {:db (assoc-in db [:answers :selections] (conj (get-in db [:answers :selections]) (:id option)))
+     :dispatch [:push-answer]}))
 
 (rf/reg-event-fx
   :page/init-home
@@ -90,14 +159,14 @@
     (-> route :data :view)))
 
 (rf/reg-sub
+  :answers
+  (fn [db _]
+    (:answers db)))
+
+(rf/reg-sub
   :quiz
   (fn [db _]
     (:quiz db)))
-
-(rf/reg-sub
-  :hey
-  (fn [db _]
-    (:hey db)))
 
 (rf/reg-sub
   :common/error
