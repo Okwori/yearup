1ae307438e7371c24a17ec34d833c7132bcfd0ff
pringwa <ayegba_24_pass>
2020-05-05 06:39:52 +0200

    updated

--- src/cljs/yearup/core.cljs
+++ src/cljs/yearup/core.cljs
@@ -7,8 +7,10 @@
     [goog.history.EventType :as HistoryEventType]
     [markdown.core :refer [md->html]]
     [reagent.core :as r]
+    [reagent.dom :as rdom]
     [reitit.core :as reitit]
     [reitit.frontend.easy :as rfe]
+    [reitit.coercion.spec :as rss]
     [re-frame.core :as rf]
     [yearup.ajax :as ajax]
     [yearup.events])
@@ -16,7 +18,165 @@
 
 (s/def ::email (s/and string? (partial re-matches #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")))
 
-(defn admin-page []   )                                     ;TODO!
+(defn admin-page []
+  ;[:div.container.body
+  ; [:div.main_container
+  ;  [:div.col-md-3.left_col
+  ;   [:div.left_col.scroll-view
+  ;    [:div.navbar.nav_title {:style {:border "0"}}
+  ;     [:a.site_title {:href "#"} [:span "YearUp Admin"]]]
+  ;    [:div.clearfix]
+  ;    [:br]
+  ;    [:div#sidebar-menu.main_menu_side.hidden-print.main_menu
+  ;     [:div.menu_section
+  ;      [:ul.nav.side-menu
+  ;       [:li [:a {:href "#"} [:i.fa.fa-dashboard] "Dashboard"]]]]]]]
+  ;  [:div.top_nav
+  ;   [:div.nav_menu
+  ;    [:div.nav.toggle
+  ;     [:a#menu_toggle [:i.fa.fa-bars]]]
+  ;    [:nav.nav.navbar-nav
+  ;     [:ul.navbar-right
+  ;      [:li.nav-item.dropdown.open {:style {:padding-left "15px"}}
+  ;       [:i.fa.fa-sign-out
+  ;        [:a#navbarDropdown.user-profile {:href "#" :aria-expanded "false"} "Logout"]]]
+  ;      ]]]]
+  ;  [:div.right_col {:role "main"}
+  ;   [:div.row {:style {:display "inline-block"}}
+  ;    [:div.animated.flipInY.col-lg-3.col-md-3.col-sm-6
+  ;     [:div.tile-stats
+  ;      [:div.icon [:i.fa.fa-user.blue]]
+  ;      [:div.count "179"]
+  ;      [:h3 "Total"]
+  ;      [:p "Number of all the users that responded to the quiz"]]]
+  ;    [:div.animated.flipInY.col-lg-3.col-md-3.col-sm-6
+  ;     [:div.tile-stats
+  ;      [:div.icon [:i.fa.fa-check.green]]
+  ;      [:div.count.green "100"]
+  ;      [:h3 "Positive"]
+  ;      [:p "Number of responses that are positive based on ratio"]]]
+  ;    [:div.animated.flipInY.col-lg-3.col-md-3.col-sm-6
+  ;     [:div.tile-stats
+  ;      [:div.icon [:i.fa.fa-thumbs-down.red]]
+  ;      [:div.count "79"]
+  ;      [:h3 "Negative"]
+  ;      [:p "Number of responses that aren't favorable based on ratio"]]]
+  ;    [:div.animated.flipInY.col-lg-3.col-md-3.col-sm-6
+  ;     [:div.tile-stats
+  ;      [:div.icon [:i.fa.fa-shield]]
+  ;      [:div.count "60%"]
+  ;      [:h3 "Ratio"]
+  ;      [:p "A percentage of user's response to be deemed positive"]]]]
+  ;   [:div.row
+  ;    [:div.col-md-12.col-sm-12
+  ;     [:div.col-md-8.col-sm-8
+  ;      [:div.x_panel
+  ;       [:div.x_title
+  ;        [:h2 "Quiz Responses"]
+  ;        [:ul.nav.navbar-right.panel_toolbox
+  ;         [:li [:a.collapse-link [:i.fa.fa-chevron-up]]]
+  ;         [:li [:a.close-link [:i.fa.fa-close]]]]
+  ;        [:div.clearfix]]
+  ;       [:div.x_content
+  ;        [:div.row
+  ;         [:div.col-sm-12
+  ;          [:div.card-box.table-responsive
+  ;           [:p.text-muted.font-13.m-b-30 "This table show all the responses to the Quiz ordered the latest responses"]
+  ;           [:table#datatable-buttons.table.table-striped.table-bordered {:style {:width "100%"}}
+  ;            [:thead
+  ;             [:tr
+  ;              [:th "Email"]
+  ;              [:th "City"]
+  ;              [:th "Date"]
+  ;              [:th "Accepted Responses?"]]]
+  ;            [:tbody
+  ;             [:tr
+  ;              [:td [:a {:href "#" :data-toggle "modal" :data-target ".bs-example-modal-lg"} "Tiger Nixon"]] [:td "System Architect"]
+  ;              [:td "System Architect"]
+  ;              [:td [:span.badge.badge-success "Yes"]]]
+  ;             [:tr
+  ;              [:td [:a {:href ""} "Simon Okwori"]]
+  ;              [:td "Accountant"]
+  ;              [:td "System Architect"]
+  ;              [:td [:span.badge.badge-danger "No"]]]]]
+  ;           [:div.modal.fade.bs-example-modal-lg {:tabIndex "-1" :role "dialog" :aria-hidden "true"}
+  ;            [:div.modal-dialog.modal-lg
+  ;             [:div.modal-content
+  ;              [:div.modal-header
+  ;               [:h4#myModalLabel.modal-title "Response"]
+  ;               [:button.close {:type "button" :data-dismiss "modal"} [:span {:aria-hidden "true"} "×"]]]
+  ;              [:div.modal-body
+  ;               [:table#datatable-buttons2.table.table-striped.table-bordered {:style {:width "100%"}}
+  ;                [:thead
+  ;                 [:tr
+  ;                  [:th "Question"]
+  ;                  [:th "Option Selected"]]]
+  ;                [:tbody
+  ;                 [:tr
+  ;                  [:td "System Architect"]
+  ;                  [:td [:span.badge.badge-success "Yes"]]]
+  ;                 [:tr
+  ;                  [:td [:a {:href ""} "Simon Okwori"]]
+  ;                  [:td [:span.badge.badge-danger "No"]]]]]]
+  ;              [:div.modal-footer
+  ;               [:button.btn.btn-secondary {:type "button" :data-dismiss "modal"} "Close"]]]]]]]]]]]
+  ;     [:div.col-md-4.col-sm-4
+  ;      [:div.x_panel.tile.fixed_height_320.overflow_hidden
+  ;       [:div.x_title
+  ;        [:h2 "Responses By City"]
+  ;        [:ul.nav.navbar-right.panel_toolbox
+  ;         [:li [:a.collapse-link [:i.fa.fa-chevron-up]]]
+  ;         [:li [:a.close-link [:i.fa.fa-close]]]]
+  ;        [:div.clearfix]]
+  ;       [:div.x_content
+  ;        [:table {:style {:width "100%"}}
+  ;         [:thead [:tr
+  ;           [:th {:style {:width "37%"}}
+  ;            [:p "&nbsp;"]]
+  ;           [:th
+  ;            [:div.col-lg-7.col-md-7.col-sm-7
+  ;             [:p "City"]]
+  ;            [:div.col-lg-5.col-md-5.col-sm-5
+  ;             [:p "% age"]]]]]
+  ;          [:tbody [:tr
+  ;           [:td
+  ;            [:canvas.canvasDoughnut {:height "140" :width "140" :style {:margin "15px 10px 10px 0"}}]]
+  ;           [:td
+  ;            [:table.tile_info
+  ;             [:tbody [:tr
+  ;               [:td
+  ;                [:p [:i.fa.fa-square.blue] "Los Angeles"]]
+  ;               [:td "35%"]]
+  ;              [:tr
+  ;               [:td
+  ;                [:p [:i.fa.fa-square.green] "New York"]]
+  ;               [:td "45%"]]
+  ;              [:tr
+  ;               [:td
+  ;                [:p [:i.fa.fa-square.purple] "Charlotte"]]
+  ;               [:td "20%"]]]]]]]]]]
+  ;      [:div.x_panel.tile.fixed_height_320.overflow_hidden
+  ;       [:div.x_title
+  ;        [:h2 "Settings"]
+  ;        [:ul.nav.navbar-right.panel_toolbox
+  ;         [:li [:a.collapse-link [:i.fa.fa-chevron-up]]]
+  ;         [:li [:a.close-link [:i.fa.fa-close]]]]
+  ;        [:div.clearfix]]
+  ;       [:div.x_content
+  ;        [:p.text-muted.font-13.m-b-30 "Adjust system settings"]
+  ;        [:form#demo-form2.form-horizontal.form-label-left {:data-parsley-validate "true"}
+  ;         [:div.item.form-group
+  ;          [:label.col-form-label.col-md-3.col-sm-3.label-align {:for "ratio-percent"} "Ratio %"]
+  ;          [:div.col-md-6.col-sm-6
+  ;           [:input#ratio-percent.form-control {:type "text" :placeholder "60"}]]]
+  ;         [:div.item.form-group
+  ;          [:div.col-md-6.col-sm-6.offset-md-3
+  ;           [:button.btn.btn-success {:type "button"} "Adjust"]]]]]]]]]
+  ;   [:br]]
+  ;  [:footer
+  ;   [:div.pull-right "© 2020 YearUp"]
+  ;   [:div.clearfix]]]]
+  )                                     ;TODO!
 
 (defn home-page []
   (when-let [quiz @(rf/subscribe [:quiz])]
@@ -86,11 +246,13 @@
                                                                (:backgroundImage question)), "')")}}]])))
 
 (defn page []
-  (if-let [page @(rf/subscribe [:page])]
-     [page]))
+  (if-let [page @(rf/subscribe [:common/page])]
+    [:div
+     ;[navbar]
+     [page]]))
 
 (defn navigate! [match _]
-  (rf/dispatch [:navigate match]))
+  (rf/dispatch [:common/navigate match]))
 
 (def router
   (reitit/router
